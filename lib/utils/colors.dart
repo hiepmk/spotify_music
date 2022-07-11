bbd98ea0f24d05d78478b83268cc5c292dd78e6d
hiepmk <hiepmk.work@gmail.com>
2022-07-11 09:40:19 +0700

    colors app

---
+++ lib/utils/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/painting.dart';
+
+// Screen Background
+const Color kBGDark = Color(0xFF171A23);
+const Color kBGLight = Color(0xFFEFEFF4);
+// Content
+const Color kBGContentDark = Color(0xFF1B2440);
+const Color kBGContentLight = Color(0xFFFFFFFF);
+// Subheads like date headers, setion headers...
+const Color kSubBGDark = Color(0xFF2A3556);
+const Color kSubBGLight = Color(0xFFEFEFF4);
+// Disable
+const Color kDisableBGDark = Color(0xFF29324D);
+const Color kDisableBGLight = Color(0xFFF0F0F0);
+// Selected
+const Color kSelectedBGDark = Color(0xFF202F61);
+const Color kSelectedBGLight = Color(0xFFE8F0FE);
